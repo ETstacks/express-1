29e4a735f5cd6ea8d3163bf91113c0e1cf16b11c
Edouard Tractere <101251237+ETstacks@users.noreply.github.com>
2022-11-21 15:35:16 +0100

    Add files via upload

---
+++ userHandlers.js
@@ -0,0 +1,73 @@
+const database = require("./database");
+
+const getUsers = (req, res) => {
+  database
+    .query("select * from users")
+    .then(([result]) => {
+      res.status(200).json(result);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+};
+
+const getUsersById = (req, res) => {
+  const id = parseInt(req.params.id);
+
+  database
+    .query("select * from users where id = ?", [id])
+    .then(([result]) => {
+      if (result[0] != null) {
+        res.status(200).json(result[0]);
+      } else {
+        res.status(404).send("Not Found");
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+};
+
+const postUser = (req, res) => {
+  const { firstname, lastname, email, city, language } = req.body;
+  database
+    .query(
+      "INSERT INTO users (firstname, lastname, email, city, language) VALUES (?, ?, ?, ?, ?)",
+      [firstname, lastname, email, city, language]
+    )
+    .then(([result]) => {
+      res.location(`/api/users/${result.insertId}`).sendStatus(201);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error saving the user");
+    });
+};
+
+const updateUser = (req, res) => {
+  const id = parseInt(req.params.id);
+  const { firstname, lastname, email, city, language } = req.body;
+  database
+    .query(
+      "update user set firstname = ?, lastname = ?, email = ?, city = ?, language = ?",
+      [firstname, lastname, email, city, language]
+    )
+    .then((result) => {
+      if (result.affectedRows === 0) {
+        res.status(404).send("Not Found");
+      } else {
+        res.sendStatus(204);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500).send("Error edited the movie");
+    });
+};
+
+module.exports = {
+  getUsers,
+  getUsersById,
+  postUser,
+  updateUser,
+};
